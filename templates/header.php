c5116e0a153cd9e9ce189a6216e1178f93d2c48e
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-05 20:09:46 -0500

    I just finished the header

--- templates/header.php
+++ templates/header.php
@@ -3,15 +3,15 @@
 
     <nav>
         <form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF'] ); ?>" method="GET">
-            <input class="search-fild" type="text" name="search">
-            <i class="fa fa-search">
-                <input type="submit" value="">
+            <input class="search-field" type="text" name="search" placeholder="Search...">
+            <i class="fa fa-search" onclick="document.querySelector('.header_submit').click()">
+                <input type="submit" class="header_submit">
             </i>
         </form>
         <ul>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+            <a href="#"><i class="fa fa-github fa-lg"></i></a>
+            <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+            <a href="#"><i class="fa fa-envelope fa-lg"></i></a>
         </ul>
     </nav>
 </header>
