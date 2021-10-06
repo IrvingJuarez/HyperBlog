d80fb9858ee1d027a9c734341809e7f393e2da92
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-05 19:14:15 -0500

    First structure of the blog

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<header>
+    <h2>Header</h2>
+
+    <nav>
+        <form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF'] ); ?>" method="GET">
+            <input class="search-fild" type="text" name="search">
+            <i class="fa fa-search">
+                <input type="submit" value="">
+            </i>
+        </form>
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </nav>
+</header>
