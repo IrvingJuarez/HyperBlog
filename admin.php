ecf26c39b2a8125b1c5ff0b2daaf3f838a7953b0
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-08 17:33:00 -0500

    The creation of the db and verifications done

--- admin.php
+++ admin.php
@@ -1,3 +1,9 @@
 <?php
 
+session_start();
+
+if( isset( $_POST['submit'] ) ){
+    echo "The form was sent";
+}
+
 require "views/admin.view.php";
--- views/admin.view.php
+++ views/admin.view.php
@@ -11,15 +11,19 @@
     <?php require "templates/header.php"; ?>
 
     <main class="admin-main">
-        <form action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] ); ?>" method="POST">
-            <label for="user">User</label>
-            <input type="text" name="user" id="user">
-
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-
-            <input type="submit" value="Access">
-        </form>
+        <?php if( isset( $_SESSION['user'] ) ): ?>
+            <h2>Welcome</h2>
+        <?php else: ?>
+            <form action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] ); ?>" method="POST">
+                <label for="user">User</label>
+                <input type="text" name="user" id="user">
+    
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+    
+                <input type="submit" name="submit" value="Access">
+            </form>
+        <?php endif; ?>
     </main>
 
     <?php require "templates/footer.php"; ?>
