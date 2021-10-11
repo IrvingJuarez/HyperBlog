0135845f50e21c822b6bc0478ba3bbbf5dfa7ceb
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-11 14:02:37 -0500

    The html for the new-article page is done

--- views/new-article.view.php
+++ views/new-article.view.php
@@ -10,6 +10,30 @@
 <body>
     <?php require "templates/header.php"; ?>
 
+    <main class="new-article_main">
+        <form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF'] ); ?>" method="POST">
+            <section>
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" placeholder="Add a title">
+            </section>
+
+            <section>
+                <label for="content">Content</label>
+                <textarea name="content" id="content" placeholder="Add the content"></textarea>
+            </section>
+
+            <section>
+                <label for="img">Add an image for the cover</label>
+                <input type="file" name="img" id="img">
+            </section>
+
+            <section>
+                <input type="submit" name="upload" value="Upload">
+                <input type="button" name="save" value="Save">
+            </section>
+        </form>
+    </main>
+
     <?php require "templates/footer.php"; ?>
 </body>
 </html>
