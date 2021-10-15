1358370114e4326ed6dbf86c0f35e077f006039a
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-14 22:26:18 -0500

    Now I have been able to access to the php file from js

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+let likeBtn = document.querySelector("button[name=like]");
+
+likeBtn.addEventListener("click", e => {
+    e.preventDefault()
+    addMessage();
+})
+
+const addMessage = () => {
+    let request = new XMLHttpRequest();
+    request.open("POST", "php/ajax.php");
+
+    request.onload = () => {
+        console.log(request.responseText)
+    }
+
+    request.send();
+}
---
+++ php/ajax.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "<script>console.log('Hi there from PHP file')</script>";
