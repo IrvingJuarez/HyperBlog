bac237500192736db0d47effcacb9e0ff3bf7144
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-14 22:34:44 -0500

    So far I got the result I am expecting, but I need to make it REALLY happen because in the database nothing is happening yet

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,5 @@
 let likeBtn = document.querySelector("button[name=like]");
+let heart = document.querySelector("button[name=like] ~ p");
 
 likeBtn.addEventListener("click", e => {
     e.preventDefault()
@@ -9,9 +10,18 @@ const addMessage = () => {
     let request = new XMLHttpRequest();
     request.open("POST", "php/ajax.php");
 
+    let props = `like=${heart}`;
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+
     request.onload = () => {
-        console.log(request.responseText)
+        heart.textContent =Number(heart.textContent) + 1;
     }
 
-    request.send();
+    request.onreadystatechange = () => {
+        if(request.readyState == 4 && request.status == 200){
+            console.log("Everything is just fine");
+        }
+    }
+
+    request.send(props);
 }
